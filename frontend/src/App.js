3660018cc55404603dceed67de9dcacaa6513c0a
Keishi Asai <isheik808@gmail.com>
2017-10-19 01:10:54 -0700

    Add SPA base

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { render } from "react-dom";
+import SideMenu from "./SideMenu";
+import Main from "./Main";
 
-class App extends Component {
+// const NotFound = () => (<div><span>NOT FOUND</span></div>);
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <SideMenu />
+        <Main />
       </div>
     );
   }
