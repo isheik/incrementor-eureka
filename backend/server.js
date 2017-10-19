7a7c0803415aefe4ca36a4d2f90ef7f136bf2ede
Keishi Asai <isheik808@gmail.com>
2017-10-18 22:15:29 -0700

    Add login API

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ var app = express();
 var bodyParser = require('body-parser');
 
 // connect DB
+// TODO: change connect method to modern one
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/backend');
 // var promise = mongoose.connect('mongodb://localhost/backend', {
@@ -33,28 +34,68 @@ app.use('/api', router);
 app.listen(port);
 console.log('listening port: ' + port);
 
-router.route('/users')
+// TODO: Review error handling and return data.
+// TODO: Password should be crypted.
+// TODO: Check whether email already exists
+/**
+ * @return token(id)
+ */
+router.route('/user/register')
     .post((req, res) => {
         var user = new User();
 
         user.mail = req.body.mail;
         user.password = req.body.password;
-        user.identifier = 1;
+        user.identifier = 0;
 
         user.save((err, user) => {
             if(err)
                 res.send(err);
-            // return message and token(id) when succefully registered
-            res.json({ message: 'User created.', token: user._id });
+            res.json(user._id);
         });
-    })
+    });
+
+// TODO: DEL this API gets all users.
+router.route('/user/all')
     .get((req, res) => {
         User.find((err, users) => {
             if(err)
                 res.send(err);
+            console.log(req.params);
             res.json(users);
         });
     });
+
+//TODO: handle email upper and lower cases
+/**
+ * Returns for each case
+ * err: return err page
+ * no user found OR
+ * password unmatch: return message
+ * else: return user token(id)
+ */
+router.route('/user/login')
+    .post((req, res) => {
+        User.findOne({ 'mail' : req.body.mail }, (err, user) => {
+            /* 
+               err: return err page
+               no user found OR
+               password unmatch: return message
+               else: return user token(id)
+            */
+            if(err)
+                res.send(err);
+            else if (user == null || req.body.password != user.password)
+                res.send({ message: 'Wrong user id or password.' });
+            else
+                res.json(user._id);
+        });
+    });
+
+// router.route('/data/nextidentifier')
+//     .post((req, res) => {
+
+//     })
 
 router.route('/users/:user_id')
     // get a user by mongo id
@@ -90,3 +131,4 @@ router.route('/users/:user_id')
             res.json({ message: 'deleted user'});
         });
     });
+
